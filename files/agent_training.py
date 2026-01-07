e1c1637c16a7f1992c8faa74053ddd592fd85fce
thomyphan <thomy.phan@ifi.lmu.de>
2026-01-07 21:16:02 +0100

    Update pages

---
+++ files/agent_training.py
@@ -0,0 +1,40 @@
+import gymnasium as gym
+import numpy as np
+from stable_baselines3 import A2C, PPO, DQN
+
+ENVIRONMENTS = ["CartPole", "Acrobot", "FrozenLake"]
+ALGORITHM = {
+    "A2C": A2C,
+    "PPO": PPO,
+    "DQN": DQN
+}
+
+# TODO set your environment and algorithm here
+environment_name = "CartPole"
+algorithm_name = "PPO"
+nr_test_episodes = 10
+
+env = gym.make(f"{environment_name}-v1", render_mode="rgb_array")
+model = ALGORITHM[algorithm_name]("MlpPolicy", env, verbose=1)
+
+# Train your agent
+model.learn(total_timesteps=10_000)
+
+# Evaluate your trained agent
+test_env = model.get_env()
+returns = []
+for e in range(nr_test_episodes):
+    done = False
+    return_value = 0.0
+    obs = test_env.reset()
+    while not done:
+        # Decision on an action
+        action, _ = model.predict(obs, deterministic=True)
+        # Execution of the action
+        obs, reward, done, info = test_env.step(action)
+
+        test_env.render("human")
+        return_value += reward
+    print(f"Episode {e}: {return_value}")
+    returns.append(return_value)
+print(f"Average return: {np.mean(returns)}")
